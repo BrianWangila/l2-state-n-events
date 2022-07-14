f6369088a9806ea5fabea9af0ca974564f3ae5c7
Brian Wangila <bwanjalah@gmail.com>
2022-07-14 12:05:35 +0300

    count up from 0

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -2,10 +2,26 @@ import React, { useState } from "react";
 
 function CountUp(){
 
+    const [count, setCount] = useState(0)
+
+    function addCount(){
+        setInterval(
+            () => { 
+                console.log(count)
+                setCount(count + 1) 
+            }, 1000 //for every second, increment count by 1
+        )
+        
+    }
+    addCount()
+
+    // TODO: FIND OUT WHY COUNTER GOES BERZERK
+    // TODO: FIND A WAY TO STOP COUNT AT 100
+
     return (
             <div>
                 <center>
-                    <h4 className="time-text">0</h4>
+                    <h4 className="time-text">{count}</h4>
                 </center>
             </div>
     )
