f8597decc65653353a482cbdd68ca955886bc1d4
Brian Wangila <bwanjalah@gmail.com>
2022-07-14 12:44:16 +0300

    count up and event listener

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -4,14 +4,29 @@ export class CountDown extends React.Component{
 
     constructor(props){
         super(props)
+        this.state = {count: 100} //initial value
     }
 
+    componentDidMount(){
+        this.update()
+    }
+
+    update(){
+        setInterval(
+            () => {
+                const currentCount = this.state.count - 1
+                this.setState({count: currentCount})
+            }, 1000
+        )
+    }
+
+        //TODO: IDENTIFY WHY COUNT REDUCES BY TWO INSTEAD OF 1
 
     render(){
         return(
             <div>
                 <center>
-                    <h4 className="time-text">100</h4>
+                    <h4 className="time-text">{this.state.count}</h4>
                 </center>
             </div>
         )
